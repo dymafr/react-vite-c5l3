7922f7b02ea7abfb2e4d15dbc8d1271198bf17e4
dymafr <contact@dyma.fr>
2023-11-02 20:26:21 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Content from './components/Content';
+import styles from './App.module.scss';
 
 function App() {
-  return <h1>Cookchef</h1>;
+  return (
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
